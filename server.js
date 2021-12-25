94ad69caa74ca65779008a5d9a2be2f2a5a81e59
vigneshkanna <vkrishitaran@gmail.com>
2021-12-25 13:05:32 +0530

    added graphiql in server

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 import express from "express";
+import resolver from "./resolver.js";
+import schema from "./schema.js";
+import { graphqlHTTP } from "express-graphql";
 
 const app = express();
 
@@ -9,3 +12,13 @@ app.listen(process.env.PORT | 5000, () => {
 app.get("/", (req, res) => {
   res.send("Hi to graphql learings");
 });
+
+const root = resolver;
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
